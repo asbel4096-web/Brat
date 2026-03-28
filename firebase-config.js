e3943b90b537bcecf946d7b5f2121dbf4659594a
asbel4096-web <asbel4096@gmail.com>
2026-03-28 02:09:53 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,10 @@
-// ضع بيانات مشروع Firebase هنا
-// مثال:
-// export const firebaseSettings = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   projectId: "...",
-//   storageBucket: "...",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
-
 export const firebaseSettings = {
-  apiKey: "AIzaSyDsZF0lFIBHFbZEs0W456Mvvmng-JrvtBE",
- 
+  apiKey: "AIzaSyDsZF0lFIBHFbZEs0W456Mvvmng-JrVtBE",
   authDomain: "bratsho-car.firebaseapp.com",
   projectId: "bratsho-car",
   storageBucket: "bratsho-car.firebasestorage.app",
   messagingSenderId: "320104607081",
-  appId: "1:320104607081:web:9fcab5d13d76206e7921f8"
+  appId: "1:320104607081:web:1f25ac69df065c4d7921f8"
 };
 
 export const adminEmails = [
